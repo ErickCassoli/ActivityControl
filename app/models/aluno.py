8213238a775762d5e6b2891434c1ab142145fd3f
ErickCassoli <erick.mcassoli@gmail.com>
2023-09-22 15:53:36 -0300

    feat subindo alteração unique em alguns models

--- app/models/aluno.py
+++ app/models/aluno.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Aluno(models.Model):
     nome = models.CharField(max_length=100)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
 
     def __str__(self):
         return self.nome
